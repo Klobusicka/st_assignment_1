770450a9b3db4fac76f9c63cfe801a5f56b61362
Klobusicka <lucie.klobusicka@gmail.com>
2025-02-26 17:36:21 +0000

    feat: basic tests for add_song function

--- test_tunepalapi.py
+++ test_tunepalapi.py
@@ -2,5 +2,26 @@ from unittest import TestCase
 from tunepalapi import TunePalAPI
 
 class TestTunePalAPI(TestCase):
+    api = TunePalAPI()
+
     def setUp(self):
         self.api = TunePalAPI()
+
+    "Testing add_song"
+    def test_add_song_one_song(self):
+        self.api.add_song("Kde si", "Elán", 2010)
+        self.assertIn("Kde si", [song.title for song in self.api.songs])
+        self.assertIn("Elán", [song.artist for song in self.api.songs])
+
+    def test_add_song_two_songs(self):
+        self.api.add_song("Nie sme zlí", "Elán", 1982)
+        self.api.add_song("Nepriznaná", "Vašo Patejdl", 1989)
+        self.assertIn("Nie sme zlí", [song.title for song in self.api.songs])
+        self.assertIn("Elán", [song.artist for song in self.api.songs])
+        self.assertIn("Nepriznaná", [song.title for song in self.api.songs])
+        self.assertIn("Vašo Patejdl", [song.artist for song in self.api.songs])
+
+    def test_add_song_twice_same_song(self):
+        self.api.add_song("Fero", "Elán", 1985)
+        self.api.add_song("Fero", "Elán", 1985)
+        self.assertIn("Fero", [song.title for song in self.api.songs])
