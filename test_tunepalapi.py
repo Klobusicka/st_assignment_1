03285a53a904fe0f7c16796cf0219b4e5a3c07c8
Klobusicka <lucie.klobusicka@gmail.com>
2025-02-28 14:31:14 +0000

    feat: tests for get_songs

--- test_tunepalapi.py
+++ test_tunepalapi.py
@@ -43,3 +43,32 @@ class TestTunePalAPI(TestCase):
         self.api.add_song("Kočka", "Ján Baláž", 1994)
         song_counts = Counter((song.title, song.artist, song.release_year) for song in self.api.songs)
         self.assertEqual(song_counts[("Kočka", "Ján Baláž", 1994)], 1)
+
+    def test_get_songs_without_page_size(self):
+        with self.assertRaises(ValueError) as context:
+            self.api.get_songs()
+
+    def test_get_songs_check_number_of_songs(self):
+        self.api.page_size = 10
+        songs_on_page = self.api.get_songs()
+        self.assertGreater(len(songs_on_page), 0)
+        self.assertEqual(len(songs_on_page), 10)
+        self.assertLessEqual(len(songs_on_page), self.api.page_size)
+
+    def test_get_songs_moving_to_next_page(self):
+        self.api.page_size = 10
+        self.api.current_page_index = 0
+        first_page = self.api.get_songs()
+        self.api.next_page()
+        second_page = self.api.get_songs()
+        self.assertNotEqual(first_page, second_page)
+
+    def test_get_songs_checking_songs_in_different_pages(self):
+        self.api.page_size = 10
+        self.api.current_page_index = 0
+        first_page = self.api.get_songs()
+        self.api.next_page()
+        second_page = self.api.get_songs()
+        first_page_songs = [song.title for song in first_page]
+        second_page_songs = [song.title for song in second_page]
+        self.assertNotEqual(first_page_songs, second_page_songs)
